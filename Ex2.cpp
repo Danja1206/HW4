2a36704e71a8fd543f026b14c9107e6deab91b25
Danja1206 <105879524+Danja1206@users.noreply.github.com>
2022-05-31 19:27:56 +0300

    Add files via upload

---
+++ Ex2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    ex2();
+}
+
+void ex2()
+{
+    vector<int> vct = { 1,2,3,4,5,6,7,8,9,1,2,66,8,6,4,2 };
+    set<int> st(vct.begin(), vct.end());
+    cout << st.size();
+}
+
